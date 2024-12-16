2cc93ab4fb0ddc6241b5d1e6a66c1c5c85d475a2
svetlana.romashevskaya <lana.romashevskaya@gmail.com>
2024-12-16 21:00:06 +0300

    Удален неиспользуемый импорт

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTest.java
@@ -6,7 +6,6 @@ import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
