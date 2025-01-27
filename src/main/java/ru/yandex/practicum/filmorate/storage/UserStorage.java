be03a0cac407da1f6ce288ef12c9503425545b15
svetlana.romashevskaya <lana.romashevskaya@gmail.com>
2025-01-27 22:21:19 +0300

    Исправлены ошибки для прохождения тестов

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -6,8 +6,12 @@ import java.util.Collection;
 
 public interface UserStorage {
     Collection<User> findAll();
+
     User getUser(Long userId);
+    
     User createUser(User newUser);
+
     User updateUser(User newUser);
+
     void deleteUser(Long userId);
 }
