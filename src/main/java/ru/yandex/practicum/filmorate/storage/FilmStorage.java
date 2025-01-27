295d1c9a1f2968a5ffd5ca4383f090d641466166
svetlana.romashevskaya <lana.romashevskaya@gmail.com>
2025-01-27 22:20:05 +0300

    Исправлены ошибки для прохождения тестов

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -6,8 +6,12 @@ import java.util.Collection;
 
 public interface FilmStorage {
     Collection<Film> findAll();
+
     Film createFilm(Film newFilm);
+
     Film updateFilm(Film newFilm);
+
     void deleteFilm(Long filmId);
+
     Film getFilm(Long filmId);
 }
