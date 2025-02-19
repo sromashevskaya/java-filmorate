5f115bdd43953f6c08b1169d7a7f9c61bba410bc
svetlana.romashevskaya <lana.romashevskaya@gmail.com>
2025-02-19 09:24:04 +0300

    Доработан код

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -4,9 +4,7 @@ import lombok.Data;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Data
 public class Film {
